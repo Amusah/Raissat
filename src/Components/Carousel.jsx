36a6502a2c9a7045c3e2cb8acd7d0afcf8e9d82a
Amusah <henryamusah@gmail.com>
2025-11-10 01:00:38 +0000

    security fix

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -11,10 +11,3 @@ const Carousel = () => {
 
 export default Carousel
 
-// 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;
-
-// https://Amusah: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@github.com/Amusah/Raissat.git
-
-// cpanel ssh pannel password: CEz1K#w-&avx
-
-// auto git deployment secret.key: a129e17ab95d4830c145b7dda43fd92b6bed2075d2375dfd
